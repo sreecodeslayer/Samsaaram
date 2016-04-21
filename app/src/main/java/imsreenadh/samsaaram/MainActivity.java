0b3448312e5987f6becef7c83ec387c1c93f81ec
Sreenadh T C <kesav.tc8@gmail.com>
2016-04-21 14:43:23 +0530

    various bug fix
    Setting Visibility Bug fixes and Malayalam Unicode format instead of raw
    text

--- app/src/main/java/imsreenadh/samsaaram/MainActivity.java
+++ app/src/main/java/imsreenadh/samsaaram/MainActivity.java
@@ -5,18 +5,22 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    ImageButton violetImageButton;
+    ImageButton redImageButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-
+        violetImageButton = (ImageButton) findViewById(R.id.samsaaramVioletIcon);
+        redImageButton = (ImageButton) findViewById(R.id.samsaaramRedIcon);
+        redImageButton.setVisibility(View.INVISIBLE); //initially this needs to be hidden to maintain proper shadow of button
         /*
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -30,17 +34,21 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    public void stopRecognition(View v) {
-        Toast.makeText(this, "സംഭാഷണം തിരിച്ചറിയല്‍ സമാപിച്ചു", Toast.LENGTH_SHORT).show();
+    public void onSamsaaramStopIconClicked(View v) {
 
-        ImageButton violetImageButton = (ImageButton) findViewById(R.id.samsaaramIcon);
+        //Handle stop recognition here
+        Toast.makeText(this, R.string.stop_toast_message, Toast.LENGTH_SHORT).show();
+        redImageButton.setVisibility(View.INVISIBLE);
         violetImageButton.setVisibility(View.VISIBLE); // hide the stopper button by showing the starter button
     }
 
-    public void onSamsaaramIconClick(View v) {
-        //Handle recognition here
-        v.setVisibility(View.INVISIBLE); //hide the starter button to show stopper button
-        Toast.makeText(this, "സംഭാഷണം തിരിച്ചറിയല്‍ ആരംഭിച്ചു", Toast.LENGTH_SHORT).show();
+    public void onSamsaaramStartIconClicked(View v) {
+        //Handle start recognition here
+        violetImageButton.setVisibility(View.INVISIBLE); //hide the starter button to show stopper button
+        redImageButton.setVisibility(View.VISIBLE);
+        TextView messageTextView = (TextView) findViewById(R.id.message);
+        messageTextView.setText(R.string.stop_message);
+        Toast.makeText(this, R.string.start_toast_message, Toast.LENGTH_SHORT).show();
     }
 
 }
